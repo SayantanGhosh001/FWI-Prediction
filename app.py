0dcbe5d6974dd449a060d15774ecaaa0e44f1e6b
SayantanGhosh001 <thesayantanghosh@gmail.com>
2023-09-07 21:22:57 +0530

    favicon added

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-application= Flask(__name__)
-app=application
+app = Flask(__name__)
 
 scaler_model = pickle.load(open('models/scaler.pkl','rb'))
 ridge_model  = pickle.load(open('models/ridge.pkl','rb'))
